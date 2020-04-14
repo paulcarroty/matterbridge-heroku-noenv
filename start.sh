d6192b310edce4df3882c050c9c4cf8c761fb695
Stafilo <38350671+Stafil0@users.noreply.github.com>
2020-04-15 03:01:43 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,4 @@
 
 lib/envsubst < config/config-heroku-template.toml > config/config-heroku.toml
 
-function _term {
-  echo "Sending SIGTERM to matterbridge"
-
-  kill --TERM "$PID" 2>/dev/null
-}
-
-trap _term SIGTERM
-
 ./matterbridge -conf=config/config-heroku.toml &
-
-PID=$!
-wait "$PID"
