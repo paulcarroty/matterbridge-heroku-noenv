f64e3c35b568cf2892708c4ea3626d29a2920dbd
Stafilo <38350671+Stafil0@users.noreply.github.com>
2020-04-15 01:10:06 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-########################################################################
-# Write Config variables in envrionment to the configuration JSON file #
-########################################################################
 lib/envsubst < config/config-heroku-template.toml > config/config-heroku.toml
 
-#####################################
-# Pass SIGTERM to Matterbridge proc #
-#####################################
 function _term {
   echo "Sending SIGTERM to matterbridge"
 
@@ -16,14 +10,8 @@ function _term {
 
 trap _term SIGTERM
 
-####################
-# Start Matterbridge #
-####################
+./bin/compile
 ./matterbridge -conf=config/config-heroku.toml &
 
 PID=$!
-
-#####################################
-# Wait for this process to complete #
-#####################################
 wait "$PID"
